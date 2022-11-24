d7c994e9398d2e85912d2c7f2e253bb19fb1b57c
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-11-24 14:33:25 +0530

    Update portifolio.py

--- portifolio.py
+++ portifolio.py
@@ -168,7 +168,7 @@ with st.container ():
         return r.json()
 
     embed_component= {'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-            <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="alan-dos-santos-eleuterio" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/alan-dos-santos-eleuterio?trk=profile-badge"></a></div>""", 'medium':"""<div style="overflow-y: auto; height:540px;"> <div id="retainable-rss-embed" 
+            <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="ashis-tiwari-2" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/ashistiwari2?trk=profile-badge"></a></div>""", 'medium':"""<div style="overflow-y: auto; height:540px;"> <div id="retainable-rss-embed" 
     data-rss="https://medium.com/feed/retainable,https://medium.com/feed/data-science-in-your-pocket"
     <div class="badge-base LI-profile-badge" 
     data-locale="pt_BR" 
